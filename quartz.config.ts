e2b8d45a699174dd68b4d6a9b473e700861915eb
Vincent Van Ouytsel <vvanouytsel@gmail.com>
2025-01-21 20:19:57 +0100

    chore: styling

--- quartz.config.ts
+++ quartz.config.ts
@@ -22,9 +22,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Exo 2",
+        body: "Open Sans",
+        code: "Victor Mono",
       },
       colors: {
         lightMode: {
